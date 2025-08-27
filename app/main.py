e7eacaacdb0cb7e33794560205808ac5025d844d
Макс <rumyantsev1m0s1@gmail.com>
2025-08-27 16:09:33 +0300

    установка poetry + базовая структура проекта

---
+++ app/main.py
@@ -0,0 +1,16 @@
+from fastapi import FastAPI
+from fastapi.responses import ORJSONResponse
+from uvicorn import run
+
+
+app = FastAPI(
+    title="Testing-Practice",
+    default_response_class=ORJSONResponse,
+)
+
+
+if __name__ == '__main__':
+    run(
+        app="app.main:app", port=8000, host="127.0.0.1",
+        reload=False, use_colors=True
+    )
