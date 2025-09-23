fb65150e5438da990b4fc7fc6089443b05cbb2c8
Макс <rumyantsev1m0s1@gmail.com>
2025-09-23 22:44:45 +0300

    добавлена проверка данных на выходе для теста проверки статус кода эндпоинта по созданию пользователя

--- tests/test_generating_id.py
+++ tests/test_generating_id.py
@@ -5,4 +5,8 @@ from httpx import AsyncClient
 @pytest.mark.anyio
 async def test_generating_id(async_client: AsyncClient) -> None:
     response = await async_client.post("/generate_user_id")
+    response_data = response.json()
+
     assert response.status_code == 201
+    assert isinstance(response_data["user_id"], int)
+    assert response_data["user_id"] > 0
