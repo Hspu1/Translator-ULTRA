fe17e7d07b614a075568c8f4bd0deb35868f3433
Макс <rumyantsev1m0s1@gmail.com>
2025-10-19 19:55:38 +0300

    Сделал код по фоновым задачам понятным + простым для тестирования

--- app/api/tasks.py
+++ app/api/tasks.py
@@ -1,10 +1,23 @@
 from sqlalchemy.exc import IntegrityError
 
+from app import TranslationModel
 from app.api.routs_logic.translator_utils import save_translated
 from app.api.schemas import TranslatedRequest
 from app.infrastructure import async_session_maker
 from app.infrastructure.queue_config import broker
 from app.utils import logger
+
+
+async def save_history_util(session, data: TranslatedRequest, obj: TranslationModel) -> bool:
+    if len(data.original_text) <= 35:
+        try:
+            async with session.begin():
+                session.add(obj)
+                return True
+
+        except IntegrityError as e:
+            logger.warning(f"IntegrityError при сохранении перевода: {e}")
+            return False
 
 
 @broker.task(
@@ -18,15 +31,10 @@ from app.utils import logger
 )
 async def save_history(data: TranslatedRequest):
     save_translated_obj = await save_translated(data=data)
-
-    if len(data.original_text) <= 35:
-        try:
-            async with async_session_maker() as session:
-                async with session.begin():
-                    session.add(save_translated_obj)
-
-        except IntegrityError as e:
-            logger.warning(f"IntegrityError при сохранении перевода: {e}")
+    async with async_session_maker() as session:
+        await save_history_util(
+            session=session, data=data, obj=save_translated_obj
+        )
 
 
 async def create_save_history_task(data: TranslatedRequest):
